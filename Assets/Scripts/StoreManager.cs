2539325b44342139936b42fef4b742d43840e08f
nankang2 <nankang2@illinois.edu>
2026-03-05 18:33:06 -0600

    Add refining machines, clicker, cooldown timers

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class StoreManager : MonoBehaviour
 {
 
     [SerializeField] private CartManager cmRef;
     [SerializeField] private GameObject cartRef;
+
+    [Header("Generator")]
     [SerializeField] private int generatorCost;
     [SerializeField] private GameObject generatorPrefab;
     [SerializeField] private Transform generatorSpawnPoint;
@@ -12,6 +15,16 @@ public class StoreManager : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI generatorCountText;
     private int generatorCount = 0;
 
+    [Header("Refining Machine")]
+    public List<GameObject> refiningMachines;
+    private int numRefiningMachines = 0;
+    [SerializeField] private TMPro.TextMeshProUGUI refiningMachineAmountText;
+    [SerializeField] private int refiningMachineCost;
+    [SerializeField] private TMPro.TextMeshProUGUI refiningMachineCostText;
+
+    [Header("Refined Ore")]
+    [SerializeField] private int refinedOreAmount = 0;
+    [SerializeField] private TMPro.TextMeshProUGUI refinedOreAmountText;
 
     public void buyGenerator()
     {
@@ -39,6 +52,45 @@ public class StoreManager : MonoBehaviour
         if (generatorCountText != null)
         {
             generatorCountText.text = generatorCount.ToString();
+        }
+    }
+
+    // This function convert Ore to Refining Machine (max num of Refining Machine = 4)
+    public void BuyRefiningMachine()
+    {
+        if (refiningMachineCost > cmRef.GetCurrentResources() || numRefiningMachines >= 4)
+        {
+            Debug.Log("Can't Buy Refining Machine");
+        }
+        else
+        {
+            cmRef.TryAddResources(-refiningMachineCost);
+            refiningMachines[numRefiningMachines].SetActive(true);
+            numRefiningMachines++;
+
+            // Update Amount
+            if (refiningMachineAmountText != null)
+            {
+                refiningMachineAmountText.text = numRefiningMachines.ToString();
+            }
+            // Update Price Tag
+            refiningMachineCost += 100;
+            if (refiningMachineCostText != null)
+            {
+                refiningMachineCostText.text = refiningMachineCost.ToString();
+            }
+            Debug.Log("1 Refining Machine Purchased");
+        }   
+    }
+
+    // Update refined ore amount when player click the machine
+    public void CollectRefinedOre(int amount)
+    {
+        // Check Timer
+        refinedOreAmount += amount;
+        if (refinedOreAmountText != null)
+        {
+            refinedOreAmountText.text = refinedOreAmount.ToString();
         }
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Production Settings")]
+    [SerializeField] private float productionTime = 5f;
+    [SerializeField] private int rewardAmount = 10;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI statusText;
+
+    [Header("References")]
+    [SerializeField] private StoreManager storeManager;
+
+    private float timer;
+    private bool isReady = false;
+
+    void Start()
+    {
+        timer = productionTime;
+        UpdateUI();
+    }
+
+    void Update()
+    {
+        if (isReady) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            timer = 0f;
+            isReady = true;
+            UpdateUI();
+        }
+        else
+        {
+            UpdateUI();
+        }
+    }
+
+    public void CollectResource()
+    {
+        if (!isReady)
+        {
+            Debug.Log("Resource is not ready yet.");
+            return;
+        }
+
+        if (storeManager != null)
+        {
+            storeManager.CollectRefinedOre(rewardAmount);
+        }
+
+        Debug.Log("Collected " + rewardAmount + " resource.");
+
+        isReady = false;
+        timer = productionTime;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (statusText == null) return;
+
+        if (isReady)
+        {
+            statusText.text = "Collect";
+        }
+        else
+        {
+            statusText.text = "Ready in: " + Mathf.Ceil(timer).ToString() + "s";
+        }
+    }
+}
