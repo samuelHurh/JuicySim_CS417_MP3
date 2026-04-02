2d5695e3580afcd729391475be77fdc05ef44c6c
Maxwell Yang <meyang2@illinois.edu>
2026-04-02 12:23:27 -0500

    Juicy Tutorial Pop-ups (2), Generators SFX (1) Particles (1), purchasing power ups (1)

--- Assets/Scripts/AccumulatorManager.cs
+++ Assets/Scripts/AccumulatorManager.cs
@@ -9,7 +9,6 @@ public class AccumulatorManager : MonoBehaviour
     [SerializeField] private CartManager cmRef;
     [SerializeField] private TextMeshProUGUI myText;
     
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
